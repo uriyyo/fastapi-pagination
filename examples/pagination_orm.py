d79343ca7d41068e00140ac943e0ba14405757da
Yurii Karabas <1998uriyyo@gmail.com>
2020-11-06 23:23:11 +0200

    🚀 Add orm integration

---
+++ examples/pagination_orm.py
@@ -0,0 +1,65 @@
+import sqlalchemy
+import uvicorn
+from databases import Database
+from fastapi import FastAPI, Depends
+from orm import Model, Integer, String
+from pydantic import BaseModel
+
+from fastapi_pagination import Page, PaginationParams
+from fastapi_pagination.ext.orm import paginate
+
+metadata = sqlalchemy.MetaData()
+db = Database("sqlite:///.db")
+
+
+class User(Model):
+    __tablename__ = "users"
+    __database__ = db
+    __metadata__ = metadata
+
+    id = Integer(primary_key=True)
+    name = String(max_length=100)
+    email = String(max_length=100)
+
+
+class UserIn(BaseModel):
+    name: str
+    email: str
+
+
+class UserOut(UserIn):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    engine = sqlalchemy.create_engine("sqlite:///.db")
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
+
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await db.disconnect()
+
+
+@app.post("/users", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    return await User.objects.create(**user_in.dict())
+
+
+@app.get("/users", response_model=Page[UserOut])
+async def get_users(params: PaginationParams = Depends()):
+    return await paginate(User.objects, params)
+
+
+if __name__ == "__main__":
+    uvicorn.run("pagination_orm:app")
---
+++ fastapi_pagination/ext/orm.py
@@ -0,0 +1,27 @@
+from orm.models import QuerySet
+
+from ..page import BasePage, create_page
+from ..params import PaginationParamsType
+
+
+async def paginate(query: QuerySet, params: PaginationParamsType) -> BasePage:
+    limit_offset_params = params.to_limit_offset()
+
+    total = await query.count()
+
+    paginated_query = (
+        query.limit(limit_offset_params.limit)
+        .build_select_expression()
+        .offset(limit_offset_params.offset)
+    )
+
+    rows = await query.database.fetch_all(paginated_query)
+    items = [
+        query.model_cls.from_row(row, select_related=query._select_related)
+        for row in rows
+    ]
+
+    return create_page(items, total, params)
+
+
+__all__ = ["paginate"]
