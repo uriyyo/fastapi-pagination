cf9daee7c8bff60d367f239913f0af849feb7962
Yurii Karabas <1998uriyyo@gmail.com>
2020-11-09 16:58:35 +0200

    🐛 Add ability to instantiate PaginationParams

--- fastapi_pagination/params.py
+++ fastapi_pagination/params.py
@@ -1,13 +1,24 @@
-from dataclasses import dataclass
-from typing import Union
+from typing import Any, Union
 
-from fastapi import Query
+from fastapi import Query, params
+from pydantic.dataclasses import dataclass
+
+
+def _get_value(val: Any) -> Any:
+    if isinstance(val, params.Query):
+        return val.default
+
+    return val
 
 
 @dataclass
 class PaginationParams:
     page: int = Query(0, ge=0, description="Page number")
     size: int = Query(50, gt=0, le=100, description="Page size")
+
+    def __post_init__(self) -> None:
+        self.page = _get_value(self.page)
+        self.size = _get_value(self.size)
 
     def to_limit_offset(self) -> "LimitOffsetPaginationParams":
         return LimitOffsetPaginationParams(
@@ -20,6 +31,10 @@ class PaginationParams:
 class LimitOffsetPaginationParams:
     limit: int = Query(50, gt=0, le=100, description="Page size limit")
     offset: int = Query(0, ge=0, description="Page offset")
+
+    def __post_init__(self) -> None:
+        self.limit = _get_value(self.limit)
+        self.offset = _get_value(self.offset)
 
     def to_limit_offset(self) -> "LimitOffsetPaginationParams":
         return self
