8e771d17620ec645924a242375556adacf3c9e72
Yurii Karabas <1998uriyyo@gmail.com>
2020-11-11 23:19:01 +0200

    🐛 Fully load query when loader present

--- fastapi_pagination/ext/gino.py
+++ fastapi_pagination/ext/gino.py
@@ -1,57 +1,27 @@
 from __future__ import annotations
 
-from contextlib import suppress
-from typing import Sequence
-
-from gino.loader import ModelLoader
 from sqlalchemy import func
 from sqlalchemy.sql import Select
 
 from ..page import BasePage, create_page
+from ..paginator import paginate as base_paginate
 from ..params import PaginationParamsType
 from .sqlalchemy import paginate_query
 
 
-async def _fetch_items(query: Select, params: PaginationParamsType) -> Sequence:
-    params = params.to_limit_offset()
-    items = []
-
-    gino = query._execution_options["loader"].model.__metadata__  # type: ignore
-
-    with suppress(StopAsyncIteration):
-        async with gino.transaction():
-            iterator = query.gino.iterate().__aiter__()  # type: ignore
-
-            for _ in range(params.offset):
-                await iterator.__anext__()
-
-            for _ in range(params.limit):
-                items.append(await iterator.__anext__())
-
-    return items
-
-
 async def paginate(query: Select, params: PaginationParamsType) -> BasePage:
-    count_query = query
-    using_loader = False
+    try:
+        is_loader_used = query._execution_options["loader"]._distinct  # type: ignore
+    except (AttributeError, KeyError):
+        is_loader_used = False
 
-    # FIXME: Should be better solution for this problem
-    # Check if it's a ModelLoader to add distinct columns to have correct count value
-    if isinstance(count_query, ModelLoader) and count_query._distinct:
-        count_query = count_query.query.distinct(*count_query._distinct)
-        using_loader = True
-    else:
-        loader = getattr(query, "_execution_options", {}).get("loader")
-        if loader and loader._distinct:
-            count_query = query.distinct(*loader._distinct)
-            using_loader = True
+    if is_loader_used:
+        # FIXME: find better way to fetch rows when loader is used
+        items = await query.gino.all()  # type: ignore
+        return base_paginate(items, params)
 
-    total = await func.count().select().select_from(count_query.alias()).gino.scalar()
-
-    if using_loader:
-        items = await _fetch_items(query, params)
-    else:
-        items = await paginate_query(query, params).gino.all()  # type: ignore
+    total = await func.count().select().select_from(query.alias()).gino.scalar()
+    items = await paginate_query(query, params).gino.all()  # type: ignore
 
     return create_page(items, total, params)
 
