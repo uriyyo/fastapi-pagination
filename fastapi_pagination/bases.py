8fb35329197baf833b99b74dd9622e334d1c40ba
Yurii Karabas <1998uriyyo@gmail.com>
2021-03-15 15:02:38 +0200

    🔧 Add Page.with_custom_options

--- fastapi_pagination/bases.py
+++ fastapi_pagination/bases.py
@@ -1,9 +1,22 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from collections import ChainMap
 from dataclasses import dataclass
-from typing import ClassVar, Generic, Sequence, Type, TypeVar
+from functools import wraps
+from typing import (
+    Any,
+    ClassVar,
+    Dict,
+    Generic,
+    Mapping,
+    Sequence,
+    Type,
+    TypeVar,
+    cast,
+)
 
+from pydantic import BaseModel, create_model
 from pydantic.generics import GenericModel
 from pydantic.types import conint
 
@@ -23,6 +36,19 @@ class AbstractParams(ABC):
         pass  # pragma: no cover
 
 
+def _create_params(cls: Type[AbstractParams], fields: Dict[str, Any]) -> Mapping[str, Any]:
+    if not issubclass(cls, BaseModel):
+        raise ValueError(f"{cls.__name__} must be subclass of BaseModel")
+
+    incorrect = sorted(fields.keys() - cls.__fields__.keys())
+    if incorrect:
+        ending = "s" if len(incorrect) > 1 else ""
+        raise ValueError(f"Unknown field{ending} {', '.join(incorrect)}")
+
+    anns = ChainMap(*(obj.__dict__.get("__annotations__", {}) for obj in cls.mro()))
+    return {name: (anns[name], val) for name, val in fields.items()}
+
+
 class AbstractPage(GenericModel, Generic[T], ABC):
     __params_type__: ClassVar[Type[AbstractParams]]
 
@@ -30,6 +56,22 @@ class AbstractPage(GenericModel, Generic[T], ABC):
     @abstractmethod
     def create(cls: Type[C], items: Sequence[T], total: int, params: AbstractParams) -> C:
         pass  # pragma: no cover
+
+    @classmethod
+    def with_custom_options(cls: C, **kwargs: Any) -> C:
+        params_cls = cast(Type[AbstractPage], cls).__params_type__
+
+        custom_params: Any = create_model(
+            params_cls.__name__,
+            __base__=params_cls,
+            **_create_params(params_cls, kwargs),
+        )
+
+        @wraps(cls, updated=())  # type: ignore
+        class CustomPage(cls[T], Generic[T]):  # type: ignore
+            __params_type__: ClassVar[Type[AbstractParams]] = custom_params
+
+        return cast(C, CustomPage)
 
     class Config:
         arbitrary_types_allowed = True
