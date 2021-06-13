1698038b5d293cdeffa9f807dcf4e135c60fafbc
Thuc Phan <kienthuc.phan@gmail.com>
2021-06-14 00:46:59 +0700

    Optimize count
    Detail: https://gist.github.com/hest/8798884

--- fastapi_pagination/ext/sqlalchemy.py
+++ fastapi_pagination/ext/sqlalchemy.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Any, Optional, TypeVar
 
+from sqlalchemy import func
 from sqlalchemy.orm import Query
 from sqlalchemy.sql import Select
 
@@ -15,6 +16,11 @@ def paginate_query(query: T, params: AbstractParams) -> T:
     raw_params = params.to_raw_params()
     return query.limit(raw_params.limit).offset(raw_params.offset)
 
+def get_count(query: T):
+    count_q = query.statement.with_only_columns([func.count()]).order_by(None)
+    count = query.session.execute(count_q).scalar()
+    return count
+
 
 def _to_dict(obj: Any) -> Any:
     try:
@@ -26,7 +32,7 @@ def _to_dict(obj: Any) -> Any:
 def paginate(query: Query, params: Optional[AbstractParams] = None) -> AbstractPage:
     params = resolve_params(params)
 
-    total = query.count()
+    total = get_count(query)
     items = [_to_dict(item) for item in paginate_query(query, params)]
 
     return create_page(items, total, params)
