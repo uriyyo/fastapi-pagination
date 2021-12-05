2a1a6acd112e9f1f0e10f1b453ef7e8d671875da
Yurii Karabas <1998uriyyo@gmail.com>
2021-12-05 18:09:42 +0200

    Add ability to prefetch related models

--- fastapi_pagination/ext/tortoise.py
+++ fastapi_pagination/ext/tortoise.py
@@ -1,13 +1,31 @@
-from typing import Optional, Type, Union
+from typing import List, Optional, Type, Union
 
 from tortoise.models import Model
+from tortoise.query_utils import Prefetch
 from tortoise.queryset import QuerySet
 
 from ..api import create_page, resolve_params
 from ..bases import AbstractPage, AbstractParams
 
 
-async def paginate(query: Union[QuerySet, Type[Model]], params: Optional[AbstractParams] = None) -> AbstractPage:
+def _generate_query(
+    query: QuerySet,
+    prefetch_related: Union[bool, List[Union[str, Prefetch]]],
+) -> QuerySet:
+    if prefetch_related:
+        if prefetch_related is True:
+            prefetch_related = [*query.model._meta.fetch_fields]
+
+        return query.prefetch_related(*prefetch_related)
+
+    return query
+
+
+async def paginate(
+    query: Union[QuerySet, Type[Model]],
+    params: Optional[AbstractParams] = None,
+    prefetch_related: Union[bool, List[Union[str, Prefetch]]] = False,
+) -> AbstractPage:
     if not isinstance(query, QuerySet):
         query = query.all()
 
@@ -16,7 +34,7 @@ async def paginate(query: Union[QuerySet, Type[Model]], params: Optional[Abstrac
     raw_params = params.to_raw_params()
 
     total = await query.count()
-    items = await query.offset(raw_params.offset).limit(raw_params.limit).all()
+    items = await _generate_query(query, prefetch_related).offset(raw_params.offset).limit(raw_params.limit).all()
 
     return create_page(items, total, params)
 
--- tests/ext/test_tortoise.py
+++ tests/ext/test_tortoise.py
@@ -1,9 +1,19 @@
+from random import choice
+from typing import List
+
 from fastapi import FastAPI
 from pytest import fixture
 from tortoise import Model
 from tortoise.backends.base.executor import EXECUTOR_CACHE
 from tortoise.contrib.fastapi import register_tortoise
-from tortoise.fields import IntField, TextField
+from tortoise.contrib.pydantic import PydanticModel
+from tortoise.fields import (
+    ForeignKeyField,
+    IntField,
+    ReverseRelation,
+    TextField,
+)
+from tortoise.query_utils import Prefetch
 
 from fastapi_pagination import Page, add_pagination
 from fastapi_pagination.ext.tortoise import paginate
@@ -13,12 +23,33 @@ from ..base import BasePaginationTestCase, UserOut
 from ..utils import faker
 
 
+class Order(Model):
+    id = IntField(pk=True)
+    name = TextField(null=False)
+    user = ForeignKeyField(f"models.User", related_name="orders")
+
+    class Meta:
+        table = "orders"
+
+
 class User(Model):
     id = IntField(pk=True)
     name = TextField(null=False)
 
+    orders: ReverseRelation[Order]
+
     class Meta:
         table = "users"
+
+
+@fixture(scope="session")
+def database_url(database_url):
+    if database_url.startswith("postgresql://"):
+        return database_url.replace("postgresql://", "postgres://")
+    if database_url.startswith("sqlite"):
+        return "sqlite://:memory:"
+
+    return database_url
 
 
 @fixture(
@@ -34,13 +65,8 @@ def query(request):
 
 
 @fixture(scope="session")
-def app(query, database_url):
+async def app(query, database_url, model_cls, pagination_params):
     app = FastAPI()
-
-    if database_url.startswith("postgresql://"):
-        database_url = database_url.replace("postgresql://", "postgres://")
-    if database_url.startswith("sqlite"):
-        database_url = "sqlite://:memory:"
 
     EXECUTOR_CACHE.clear()
     register_tortoise(
@@ -50,10 +76,10 @@ def app(query, database_url):
         generate_schemas=True,
     )
 
-    @app.get("/default", response_model=Page[UserOut])
-    @app.get("/limit-offset", response_model=LimitOffsetPage[UserOut])
+    @app.get("/default", response_model=Page[model_cls])
+    @app.get("/limit-offset", response_model=LimitOffsetPage[model_cls])
     async def route():
-        return await paginate(query)
+        return await paginate(query, **pagination_params())
 
     add_pagination(app)
     return app
@@ -61,9 +87,53 @@ def app(query, database_url):
 
 class TestTortoise(BasePaginationTestCase):
     @fixture(scope="session")
+    def pagination_params(self):
+        return lambda: {"prefetch_related": False}
+
+    @fixture(scope="session")
+    def model_cls(self):
+        return UserOut
+
+    @fixture(scope="session")
     async def entities(self, query, client):
         await User.all().delete()
-        for _ in range(100):
-            await User.create(name=faker.name())
+        await User.bulk_create(User(name=faker.name()) for _ in range(100))
 
         return await User.all()
+
+
+class OrderOut(PydanticModel):
+    name: str
+
+
+class UserWithRelationOut(PydanticModel):
+    name: str
+    orders: List[OrderOut]
+
+
+class TestTortoiseWithRelatedObjects(BasePaginationTestCase):
+    model = UserWithRelationOut
+
+    @fixture(
+        scope="session",
+        params=[True, ["orders"], "prefetch"],
+        ids=["bool", "list", "prefetch"],
+    )
+    def pagination_params(self, request):
+        return lambda: {
+            "prefetch_related": [Prefetch("orders", Order.all())] if request.param == "prefetch" else request.param
+        }
+
+    @fixture(scope="session")
+    def model_cls(self):
+        return self.model
+
+    @fixture(scope="session")
+    async def entities(self, query, client):
+        await User.all().delete()
+        await User.bulk_create(User(name=faker.name()) for _ in range(100))
+
+        users = await User.all()
+        await Order.bulk_create(Order(name=faker.name(), user=choice(users)) for _ in range(300))
+
+        return await User.all().prefetch_related("orders")
