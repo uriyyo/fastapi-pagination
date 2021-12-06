5759544a6b565e7a3df26d6a74d174941fe0d6d8
Yurii Karabas <1998uriyyo@gmail.com>
2021-12-06 10:55:01 +0200

    🔧   Add ability to prefetch related models for tortoise (#243)

--- tests/ext/test_tortoise.py
+++ tests/ext/test_tortoise.py
@@ -1,24 +1,55 @@
+from random import choice
+from typing import List
+
 from fastapi import FastAPI
 from pytest import fixture
 from tortoise import Model
 from tortoise.backends.base.executor import EXECUTOR_CACHE
 from tortoise.contrib.fastapi import register_tortoise
-from tortoise.fields import IntField, TextField
+from tortoise.contrib.pydantic import PydanticModel
+from tortoise.fields import (
+    ForeignKeyField,
+    IntField,
+    ReverseRelation,
+    TextField,
+)
+from tortoise.query_utils import Prefetch
 
 from fastapi_pagination import Page, add_pagination
 from fastapi_pagination.ext.tortoise import paginate
 from fastapi_pagination.limit_offset import Page as LimitOffsetPage
 
-from ..base import BasePaginationTestCase, UserOut
+from ..base import BasePaginationTestCase
 from ..utils import faker
+
+
+class Order(Model):
+    id = IntField(pk=True)
+    name = TextField(null=False)
+    user = ForeignKeyField(f"models.User", related_name="orders")
+
+    class Meta:
+        table = "orders"
 
 
 class User(Model):
     id = IntField(pk=True)
     name = TextField(null=False)
 
+    orders: ReverseRelation[Order]
+
     class Meta:
         table = "users"
+
+
+@fixture(scope="session")
+def database_url(database_url):
+    if database_url.startswith("postgresql://"):
+        return database_url.replace("postgresql://", "postgres://")
+    if database_url.startswith("sqlite"):
+        return "sqlite://:memory:"
+
+    return database_url
 
 
 @fixture(
@@ -28,42 +59,76 @@ class User(Model):
 )
 def query(request):
     if request.param:
-        return User
+        return lambda: User
     else:
-        return User.all()
+        return lambda: User.all()
 
 
-@fixture(scope="session")
-def app(query, database_url):
-    app = FastAPI()
+class BaseTortoiseTestCase(BasePaginationTestCase):
+    @fixture(scope="session")
+    def model_cls(self):
+        return self.model
 
-    if database_url.startswith("postgresql://"):
-        database_url = database_url.replace("postgresql://", "postgres://")
-    if database_url.startswith("sqlite"):
-        database_url = "sqlite://:memory:"
+    @fixture(scope="session")
+    async def app(self, database_url, query, model_cls, pagination_params):
+        app = FastAPI()
 
-    EXECUTOR_CACHE.clear()
-    register_tortoise(
-        app,
-        modules={"models": [__name__]},
-        db_url=database_url,
-        generate_schemas=True,
-    )
+        EXECUTOR_CACHE.clear()
+        register_tortoise(
+            app,
+            modules={"models": [__name__]},
+            db_url=database_url,
+            generate_schemas=True,
+        )
 
-    @app.get("/default", response_model=Page[UserOut])
-    @app.get("/limit-offset", response_model=LimitOffsetPage[UserOut])
-    async def route():
-        return await paginate(query)
+        @app.get("/default", response_model=Page[model_cls])
+        @app.get("/limit-offset", response_model=LimitOffsetPage[model_cls])
+        async def route():
+            return await paginate(query(), **pagination_params())
 
-    add_pagination(app)
-    return app
+        add_pagination(app)
+        return app
 
 
-class TestTortoise(BasePaginationTestCase):
+class TestTortoise(BaseTortoiseTestCase):
+    @fixture(scope="session")
+    def pagination_params(self):
+        return lambda: {"prefetch_related": False}
+
     @fixture(scope="session")
     async def entities(self, query, client):
         await User.all().delete()
-        for _ in range(100):
-            await User.create(name=faker.name())
+        await User.bulk_create(User(name=faker.name()) for _ in range(100))
 
         return await User.all()
+
+
+class OrderOut(PydanticModel):
+    name: str
+
+
+class UserWithRelationOut(PydanticModel):
+    name: str
+    orders: List[OrderOut]
+
+
+class TestTortoiseWithRelatedObjects(BaseTortoiseTestCase):
+    model = UserWithRelationOut
+
+    @fixture(
+        scope="session",
+        params=[lambda: True, lambda: ["orders"], lambda: [Prefetch("orders", Order.all())]],
+        ids=["bool", "list", "prefetch"],
+    )
+    def pagination_params(self, request):
+        return lambda: {"prefetch_related": request.param()}
+
+    @fixture(scope="session")
+    async def entities(self, query, client):
+        await User.all().delete()
+        await User.bulk_create(User(name=faker.name()) for _ in range(100))
+
+        users = await User.all()
+        await Order.bulk_create(Order(name=faker.name(), user=choice(users)) for _ in range(300))
+
+        return await User.all().prefetch_related("orders")
