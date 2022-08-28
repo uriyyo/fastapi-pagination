3d836b722c0442ab85935d229a729f6112070c3c
Yurii Karabas <1998uriyyo@gmail.com>
2022-08-28 19:52:32 +0300

    Add ability to paginate not only scalars

--- fastapi_pagination/ext/async_sqlalchemy.py
+++ fastapi_pagination/ext/async_sqlalchemy.py
@@ -7,23 +7,25 @@ from sqlalchemy import func, select
 from ..api import create_page, resolve_params
 from ..bases import AbstractPage, AbstractParams
 from .sqlalchemy import paginate_query
+from .utils import unwrap_scalars
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from sqlalchemy.ext.asyncio.engine import AsyncConnectable
     from sqlalchemy.sql import Select
 
 
 async def paginate(
-    session: AsyncSession,
+    conn: AsyncConnectable | AsyncSession,
     query: Select,
     params: Optional[AbstractParams] = None,
 ) -> AbstractPage:  # pragma: no cover # FIXME: fix coverage report generation
     params = resolve_params(params)
 
-    total = await session.scalar(select(func.count()).select_from(query.subquery()))  # type: ignore
-    items = await session.execute(paginate_query(query, params))
+    total = await conn.scalar(select(func.count()).select_from(query.subquery()))  # type: ignore
+    items = await conn.execute(paginate_query(query, params))
 
-    return create_page(items.scalars().unique().all(), total, params)
+    return create_page(unwrap_scalars(items.unique().all()), total, params)
 
 
 __all__ = ["paginate"]
--- fastapi_pagination/ext/sqlalchemy_future.py
+++ fastapi_pagination/ext/sqlalchemy_future.py
@@ -2,15 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional, Union
 
-from sqlalchemy import func, select
-from sqlalchemy.orm import Session
+from sqlalchemy import func, literal_column, select
 
 from ..api import create_page, resolve_params
 from ..bases import AbstractPage, AbstractParams
 from .sqlalchemy import paginate_query
+from .utils import unwrap_scalars
 
 if TYPE_CHECKING:
     from sqlalchemy.future import Connection, Engine, Select
+    from sqlalchemy.orm import Session
 
 
 def paginate(
@@ -20,10 +21,10 @@ def paginate(
 ) -> AbstractPage:
     params = resolve_params(params)
 
-    total = conn.scalar(select(func.count()).select_from(query.subquery()))
+    total = conn.scalar(select(func.count(literal_column("*"))).select_from(query.subquery()))
     items = conn.execute(paginate_query(query, params))
 
-    return create_page(items.scalars().unique().all(), total, params)
+    return create_page(unwrap_scalars(items.unique().all()), total, params)
 
 
 __all__ = ["paginate"]
