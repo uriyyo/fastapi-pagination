dc7f45a79ac1289e58263e2c4d5beb6ebe1d2ba6
Anthony Uk <anthony.uk@hirslanden.ch>
2023-03-07 16:39:06 +0100

    Extend beanie.py: pass through all possible parameters for find_many; paginate aggregate queries using $facets (same approach as in motor.py)

--- fastapi_pagination/ext/beanie.py
+++ fastapi_pagination/ext/beanie.py
@@ -1,8 +1,11 @@
 __all__ = ["paginate"]
 
-from typing import Optional, TypeVar, Union, Any
+from typing import List, Tuple, Optional, Type, TypeVar, Union, Any
 
 from beanie import Document
+from beanie.odm.enums import SortDirection
+from beanie.odm.interfaces.aggregate import DocumentProjectionType, ClientSession
+from beanie.odm.queries.aggregation import AggregationQuery
 from beanie.odm.queries.find import FindMany
 
 from ..api import create_page
@@ -13,16 +16,60 @@ from ..utils import verify_params
 TDocument = TypeVar("TDocument", bound=Document)
 
 
+
 async def paginate(
-    query: Union[TDocument, FindMany[TDocument]],
+    query: Union[TDocument, FindMany[TDocument], AggregationQuery[TDocument]],
     params: Optional[AbstractParams] = None,
     *,
     additional_data: AdditionalData = None,
+    projection_model: Optional[Type[DocumentProjectionType]] = None,
+    sort: Union[None, str, List[Tuple[str, SortDirection]]] = None,
+    session: Optional[ClientSession] = None,
+    ignore_cache: bool = False,
     fetch_links: bool = False,
+    with_children: bool = False,
+    lazy_parse: bool = False,
+    **pymongo_kwargs
 ) -> Any:
     params, raw_params = verify_params(params, "limit-offset")
 
-    items = await query.find_many(limit=raw_params.limit, skip=raw_params.offset, fetch_links=fetch_links).to_list()
-    total = await query.find({}, fetch_links=False).count()
+    if isinstance(query, AggregationQuery):
+        aggregation_query = query.clone()
+        aggregation_query.aggregation_pipeline.extend([
+            {
+                "$facet": {
+                    "metadata": [{"$count": "total"}],
+                    "data": [
+                        {"$limit": raw_params.limit + raw_params.offset},
+                        {"$skip": raw_params.offset},
+                    ],
+                }
+            },
+        ])
+        data = (await aggregation_query.to_list())[0]
+        items = data["data"]
+        try:
+            total = data["metadata"][0]["total"]
+        except IndexError:
+            total = 0
+    else:
+        items = await query.find_many(
+            limit=raw_params.limit,
+            skip=raw_params.offset,
+            projection_model=projection_model,
+            sort=sort,
+            session=session,
+            ignore_cache=ignore_cache,
+            fetch_links=fetch_links,
+            with_children=with_children,
+            lazy_parse=lazy_parse,
+            **pymongo_kwargs
+        ).to_list()
+        total = await query.find({},
+            session=session,
+            ignore_cache=ignore_cache,
+            fetch_links=False,
+            **pymongo_kwargs
+        ).count()
 
     return create_page(items, total, params, **(additional_data or {}))
