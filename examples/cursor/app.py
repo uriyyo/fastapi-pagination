f310fd8d81cb1fa4ac9db42bb51e0e5e75f7fb9a
Yurii Karabas <1998uriyyo@gmail.com>
2024-06-06 17:20:00 +0200

    Update cursor example

--- examples/cursor/app.py
+++ examples/cursor/app.py
@@ -1,5 +1,6 @@
 from datetime import date
-from typing import Any, Iterator, Dict
+from pathlib import Path
+from typing import Any, Iterator, Dict, TypeVar
 
 from faker import Faker
 from fastapi import Depends, FastAPI, Request
@@ -8,17 +9,27 @@ from fastapi.templating import Jinja2Templates
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 from fastapi_pagination import pagination_ctx, resolve_params
-from fastapi_pagination.cursor import CursorPage
+from fastapi_pagination.cursor import CursorPage as BaseCursorPage
+from fastapi_pagination.customization import CustomizedPage, UseIncludeTotal, UseParamsFields
 from fastapi_pagination.ext.sqlmodel import paginate
+
+ROOT = Path(__file__).parent
 
 fake = Faker()
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=ROOT / "templates")
 
 engine = create_engine("sqlite:///.db")
 
-CursorPage = CursorPage.with_custom_options(size=10)
+
+T = TypeVar("T")
+
+CursorPage = CustomizedPage[
+    BaseCursorPage[T],
+    UseIncludeTotal(True),
+    UseParamsFields(size=10),
+]
 
 
 class User(SQLModel, table=True):
@@ -40,7 +51,7 @@ def get_db() -> Iterator[Session]:
 def user_data(id_: int) -> Dict[str, Any]:
     return {
         "id": id_ + 1,
-        "profile_pic": f"https://avatars.dicebear.com/api/croodles/{id_}.svg",
+        "profile_pic": f"https://api.dicebear.com/8.x/croodles/svg?seed={id_ + 1}",
         "first_name": fake.first_name(),
         "last_name": fake.last_name(),
         "email": fake.email(),
@@ -53,8 +64,10 @@ def on_startup():
     User.metadata.drop_all(engine)
     User.metadata.create_all(engine)
 
+    total = fake.pyint(100, 200)
+
     with Session(engine) as session:
-        session.add_all([User(**user_data(i)) for i in range(100)])
+        session.add_all([User(**user_data(i)) for i in range(total)])
         session.commit()
 
 
