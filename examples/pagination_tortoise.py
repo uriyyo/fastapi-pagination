864456b78933570c54b3779958d74f6de165db74
Waket Zheng <waketzheng@gmail.com>
2024-06-12 11:45:53 +0800

    Update tortoise orm example

--- examples/pagination_tortoise.py
+++ examples/pagination_tortoise.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python
 from contextlib import asynccontextmanager
-from typing import Any
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, AsyncGenerator
 
 import uvicorn
 from faker import Faker
 from fastapi import FastAPI
-from pydantic import BaseModel
-from tortoise import Model
-from tortoise.contrib.fastapi import register_tortoise
-from tortoise.fields import IntField, TextField
-
 from fastapi_pagination import LimitOffsetPage, Page, add_pagination
 from fastapi_pagination.ext.tortoise import paginate
+from tortoise import Model
+from tortoise.contrib.fastapi import RegisterTortoise  # requires 'tortoise-orm>0.21.0'
+from tortoise.contrib.pydantic import PydanticModel, pydantic_model_creator
+from tortoise.fields import IntField, TextField
 
 faker = Faker()
 
@@ -21,34 +22,40 @@ class User(Model):
     email = TextField(null=False)
 
 
-class UserIn(BaseModel):
-    name: str
-    email: str
+if TYPE_CHECKING:  # pragma: nocoverage
+
+    class UserIn(User, PydanticModel):  # type:ignore[misc]
+        pass
+
+    class UserOut(User, PydanticModel):  # type:ignore[misc]
+        pass
+else:
+    UserIn = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)
+    UserOut = pydantic_model_creator(User, name="User")
 
 
-class UserOut(UserIn):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url="sqlite://:memory:",
-    modules={"models": [__name__]},
-    generate_schemas=True,
-)
-
-@app.on_event("startup")
-async def on_startup() -> None:
+async def _initial_users() -> None:
     for _ in range(100):
         await User.create(
             name=faker.name(),
             email=faker.email(),
         )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    async with RegisterTortoise(
+        app,
+        db_url="sqlite://:memory:",
+        modules={"models": [__name__]},
+        generate_schemas=True,
+    ):
+        await _initial_users()
+        add_pagination(app)
+        yield
+
+
+app = FastAPI(title="Tortoise ORM Pagination example", lifespan=lifespan)
 
 
 @app.post("/users", response_model=UserOut)
@@ -62,7 +69,5 @@ async def get_users() -> Any:
     return await paginate(User)
 
 
-add_pagination(app)
-
 if __name__ == "__main__":
-    uvicorn.run("pagination_tortoise:app")
+    uvicorn.run(f"{Path(__file__).stem}:app")
