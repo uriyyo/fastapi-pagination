0d65eb581d0061e28e99b402381eb16014ea2891
Yurii Karabas <1998uriyyo@gmail.com>
2024-09-10 17:26:15 +0200

    Remove unused code

--- tests/patch.py
+++ tests/patch.py
@@ -1,19 +1,5 @@
-import asyncio
-from functools import lru_cache
-
 from pydantic import generics
 
 # https://github.com/pydantic/pydantic/issues/4483
 generics._generic_types_cache = {}
 generics._assigned_parameters = {}
-
-
-@lru_cache()
-def _get_event_loop_no_warn(*_):
-    loop = asyncio.new_event_loop()
-    loop.__pytest_asyncio = True
-
-    return loop
-
-
-# plugin._get_event_loop_no_warn = _get_event_loop_no_warn
