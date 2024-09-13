00f661c607e1f742af399679c489271da014d98f
Yurii Karabas <1998uriyyo@gmail.com>
2024-09-13 20:34:16 +0200

    Add tests for sqlachemy scalar unwrap logic

--- tests/ext/test_sqlalchemy.py
+++ tests/ext/test_sqlalchemy.py
@@ -1,14 +1,16 @@
-from typing import Iterator, Type
+from contextlib import closing
+from typing import Any, Iterator, Type
 
 from fastapi import Depends, FastAPI
-from pytest import fixture, skip
+from pytest import fixture, mark, skip
 from sqlalchemy import select
 from sqlalchemy.orm.session import Session
 
-from fastapi_pagination import LimitOffsetPage, Page, add_pagination
+from fastapi_pagination import LimitOffsetPage, Page, Params, add_pagination, set_page
 from fastapi_pagination.ext.sqlalchemy import paginate
 
 from ..base import BasePaginationTestCase
+from ..schemas import UserOut, UserWithoutIDOut
 from .utils import is_sqlalchemy20, sqlalchemy20
 
 
@@ -45,4 +47,40 @@ def app(sa_user, sa_session: Type[Session], model_cls: Type[object], use_subquer
 
 @sqlalchemy20
 class TestSQLAlchemy(BasePaginationTestCase):
-    pass
+    def test_scalar_not_unwrapped(self, sa_session, sa_user, entities):
+        with closing(sa_session()) as session, set_page(Page[UserWithoutIDOut]):
+            page = paginate(session, select(sa_user.name), params=Params(page=1, size=10))
+
+        assert page.dict()["items"] == [{"name": entry.name} for entry in entities[:10]]
+
+    def test_non_scalar_not_unwrapped(self, sa_session, sa_user, entities):
+        with closing(sa_session()) as session, set_page(Page[UserOut]):
+            page = paginate(session, select(sa_user.id, sa_user.name), params=Params(page=1, size=10))
+
+        assert page.dict()["items"] == [{"id": entry.id, "name": entry.name} for entry in entities[:10]]
+
+    @mark.parametrize(
+        ("query", "validate"),
+        [
+            (
+                lambda sa_user: select(sa_user),
+                lambda sa_user, item: isinstance(item, sa_user),
+            ),
+            (
+                lambda sa_user: select(sa_user.id),
+                lambda sa_user, item: len(item) == 1,
+            ),
+            (
+                lambda sa_user: select(sa_user.id, sa_user.name),
+                lambda sa_user, item: len(item) == 2,
+            ),
+        ],
+    )
+    def test_unwrap_raw_results(self, sa_session, sa_user, query, validate):
+        q = query(sa_user)
+
+        with closing(sa_session()) as session, set_page(Page[Any]):
+            page = paginate(session, q, params=Params(page=1, size=1))
+
+        assert page.items
+        assert validate(sa_user, page.items[0])
