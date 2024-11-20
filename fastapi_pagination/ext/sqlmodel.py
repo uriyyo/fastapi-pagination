440c0dac5b7c486c67c0ea8eb35b77dff1e5d282
Yurii Karabas <1998uriyyo@gmail.com>
2024-11-20 11:46:07 +0100

    Add BaseSelect type hint for SQLModel paginate

--- fastapi_pagination/ext/sqlmodel.py
+++ fastapi_pagination/ext/sqlmodel.py
@@ -1,6 +1,6 @@
 __all__ = ["paginate"]
 
-from typing import Any, Optional, Type, TypeVar, Union, no_type_check, overload
+from typing import Any, Generic, Optional, Type, TypeVar, Union, no_type_check, overload
 
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
 from sqlmodel import Session, SQLModel, select
@@ -9,6 +9,15 @@ from sqlmodel.sql.expression import Select, SelectOfScalar
 from ..bases import AbstractParams
 from ..types import AdditionalData, AsyncItemsTransformer, ItemsTransformer, SyncItemsTransformer
 from .sqlalchemy import paginate as _paginate
+
+try:
+    from sqlmodel.sql._expression_select_cls import SelectBase
+except ImportError:
+    _T = TypeVar("_T")
+
+    class SelectBase(Generic[_T]):  # type: ignore[no-redef]
+        pass
+
 
 T = TypeVar("T")
 TSQLModel = TypeVar("TSQLModel", bound=SQLModel)
@@ -60,6 +69,21 @@ def paginate(
 
 
 @overload
+def paginate(
+    session: Session,
+    query: SelectBase[TSQLModel],
+    params: Optional[AbstractParams] = None,
+    *,
+    count_query: Optional[TSQLModel] = None,
+    subquery_count: bool = True,
+    transformer: Optional[SyncItemsTransformer] = None,
+    additional_data: Optional[AdditionalData] = None,
+    unique: bool = True,
+) -> Any:
+    pass
+
+
+@overload
 async def paginate(
     session: Union[AsyncSession, AsyncConnection],
     query: Select[TSQLModel],
@@ -93,6 +117,20 @@ async def paginate(
 async def paginate(
     session: Union[AsyncSession, AsyncConnection],
     query: Type[TSQLModel],
+    params: Optional[AbstractParams] = None,
+    *,
+    count_query: Optional[Type[TSQLModel]] = None,
+    subquery_count: bool = True,
+    transformer: Optional[AsyncItemsTransformer] = None,
+    additional_data: Optional[AdditionalData] = None,
+) -> Any:
+    pass
+
+
+@overload
+async def paginate(
+    session: Union[AsyncSession, AsyncConnection],
+    query: SelectBase[TSQLModel],
     params: Optional[AbstractParams] = None,
     *,
     count_query: Optional[Type[TSQLModel]] = None,
