36e6747e07020a24820337f7ec75506bec94d463
Yurii Karabas <1998uriyyo@gmail.com>
2025-01-04 20:06:59 +0100

    Add firestore cursor paginate

--- fastapi_pagination/ext/firestore.py
+++ fastapi_pagination/ext/firestore.py
@@ -3,7 +3,7 @@ __all__ = [
     "paginate",
 ]
 
-from typing import Any, Optional, TypeVar, Union
+from typing import Any, Optional, TypeVar, Union, cast
 
 from google.cloud.firestore_v1 import (
     AsyncCollectionReference,
@@ -18,7 +18,7 @@ from google.cloud.firestore_v1.aggregation import AggregationQuery
 from google.cloud.firestore_v1.async_aggregation import AsyncAggregationQuery
 
 from fastapi_pagination.api import apply_items_transformer, create_page
-from fastapi_pagination.bases import AbstractParams, RawParams
+from fastapi_pagination.bases import AbstractParams, CursorRawParams, RawParams, is_cursor
 from fastapi_pagination.types import AdditionalData, ItemsTransformer, SyncItemsTransformer
 from fastapi_pagination.utils import verify_params
 
@@ -33,8 +33,22 @@ def _apply_limit_offset(query: TQuery, params: RawParams) -> TQuery:
     return query
 
 
+def _apply_cursor(
+    query: TQuery,
+    params: CursorRawParams,
+    snapshot: Optional[DocumentSnapshot],
+) -> TQuery:
+    if snapshot is not None:
+        query = query.start_after(snapshot)
+    if params.size is not None:
+        query = query.limit(params.size)
+    return query
+
+
 def _convert_raw_items(
-    items: list[DocumentSnapshot], *, raw: bool
+    items: list[DocumentSnapshot],
+    *,
+    raw: bool,
 ) -> Union[list[DocumentSnapshot], list[dict[str, Any]]]:
     if raw:
         return items
@@ -52,18 +66,33 @@ def paginate(
     transformer: Optional[SyncItemsTransformer] = None,
     additional_data: Optional[AdditionalData] = None,
 ) -> Any:
-    params, raw_params = verify_params(params, "limit-offset")
+    params, raw_params = verify_params(params, "limit-offset", "cursor")
+    additional_data = additional_data or {}
 
-    if isinstance(src, CollectionReference):
-        src = Query(src)
+    if isinstance(src, CollectionReference):  # noqa: SIM108
+        query = Query(src)
+    else:
+        query = src
 
     total = None
     if raw_params.include_total:
-        total_res = AggregationQuery(src).count("total").get(transaction=transaction)
+        total_res = AggregationQuery(query).count("total").get(transaction=transaction)
         total = total_res[0][0].value
 
-    query = _apply_limit_offset(src, raw_params)
+    if is_cursor(raw_params):
+        snapshot = None
+        if cursor := raw_params.cursor:
+            snapshot = cast(DocumentSnapshot, query._parent.document(cursor).get(transaction=transaction))
+
+        query = _apply_cursor(query, raw_params, snapshot)
+    else:
+        query = _apply_limit_offset(query, raw_params.as_limit_offset())
+
     raw_items = query.get(transaction=transaction)
+
+    if is_cursor(raw_params) and raw_items:
+        additional_data["next_"] = raw_items[-1].id
+
     items = _convert_raw_items(raw_items, raw=raw)
     t_items = apply_items_transformer(items, transformer)
 
@@ -71,7 +100,7 @@ def paginate(
         t_items,
         total=total,
         params=params,
-        **(additional_data or {}),
+        **additional_data,
     )
 
 
@@ -85,18 +114,33 @@ async def apaginate(
     transformer: Optional[ItemsTransformer] = None,
     additional_data: Optional[AdditionalData] = None,
 ) -> Any:
-    params, raw_params = verify_params(params, "limit-offset")
+    params, raw_params = verify_params(params, "limit-offset", "cursor")
+    additional_data = additional_data or {}
 
-    if isinstance(src, AsyncCollectionReference):
-        src = AsyncQuery(src)
+    if isinstance(src, AsyncCollectionReference):  # noqa: SIM108
+        query = AsyncQuery(src)
+    else:
+        query = src
 
     total = None
     if raw_params.include_total:
-        total_res = await AsyncAggregationQuery(src).count("total").get(transaction=transaction)
+        total_res = await AsyncAggregationQuery(query).count("total").get(transaction=transaction)
         total = total_res[0][0].value
 
-    query = _apply_limit_offset(src, raw_params)
+    if is_cursor(raw_params):  # noqa: SIM108
+        snapshot = None
+        if cursor := raw_params.cursor:
+            snapshot = cast(DocumentSnapshot, await query._parent.document(cursor).get(transaction=transaction))
+
+        query = _apply_cursor(query, raw_params, snapshot)
+    else:
+        query = _apply_limit_offset(query, raw_params.as_limit_offset())
+
     raw_items = await query.get(transaction=transaction)  # type: ignore[arg-type]
+
+    if is_cursor(raw_params) and raw_items:
+        additional_data["next_"] = raw_items[-1].id
+
     items = _convert_raw_items(raw_items, raw=raw)
     t_items = await apply_items_transformer(items, transformer, async_=True)
 
@@ -104,5 +148,5 @@ async def apaginate(
         t_items,
         total=total,
         params=params,
-        **(additional_data or {}),
+        **additional_data,
     )
--- tests/ext/test_firestore.py
+++ tests/ext/test_firestore.py
@@ -5,8 +5,7 @@ from tests.base import BasePaginationTestSuite, async_sync_testsuite
 from tests.utils import maybe_async
 
 
-@async_sync_testsuite
-class TestFirestore(BasePaginationTestSuite):
+class _BaseFirestoreSuite:
     @pytest.fixture(scope="session")
     def firestore(self, firestore_project_id, is_async_db):
         from google.cloud import firestore as _firestore
@@ -24,11 +23,30 @@ class TestFirestore(BasePaginationTestSuite):
     def entities(self, entities):
         return sorted(entities, key=lambda x: x.name)
 
+
+@async_sync_testsuite
+class TestFirestore(_BaseFirestoreSuite, BasePaginationTestSuite):
     @pytest.fixture(scope="session")
     def app(self, builder, firestore, paginate_func):
         builder = builder.new()
 
         @builder.both.default
+        async def route():
+            return await maybe_async(paginate_func(firestore.collection("users").order_by("name")))
+
+        return builder.build()
+
+
+@async_sync_testsuite
+class TestCursorFirestore(_BaseFirestoreSuite, BasePaginationTestSuite):
+    def _prepare_cursor_entities(self, entities):
+        return sorted(entities, key=lambda x: x.name)
+
+    @pytest.fixture(scope="session")
+    def app(self, builder, firestore, paginate_func):
+        builder = builder.new()
+
+        @builder.cursor.default
         async def route():
             return await maybe_async(paginate_func(firestore.collection("users").order_by("name")))
 
