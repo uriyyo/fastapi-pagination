ed2f5f25b94597c6580dc57ba3d6bccdc1326a6f
Yurii Karabas <1998uriyyo@gmail.com>
2025-06-25 23:22:29 +0200

    Add pymongo aggregate funcs (#1553)

--- tests/ext/test_pymongo.py
+++ tests/ext/test_pymongo.py
@@ -2,7 +2,7 @@ import pytest
 from pymongo import AsyncMongoClient, MongoClient
 from pytest_asyncio import fixture as async_fixture
 
-from fastapi_pagination.ext.pymongo import apaginate, paginate
+from fastapi_pagination.ext.pymongo import apaginate, apaginate_aggregate, paginate, paginate_aggregate
 from tests.base import BasePaginationTestSuite, async_sync_testsuite
 from tests.utils import maybe_async
 
@@ -14,9 +14,8 @@ def database_url(mongodb_url) -> str:
     return mongodb_url
 
 
-@mongodb_test
 @async_sync_testsuite
-class TestPymongo(BasePaginationTestSuite):
+class _BasePymongoSuite:
     @async_fixture(scope="session")
     async def db_client(self, database_url, is_async_db):
         if is_async_db:
@@ -26,6 +25,9 @@ class TestPymongo(BasePaginationTestSuite):
             with MongoClient(database_url) as client:
                 yield client
 
+
+@mongodb_test
+class TestPymongo(_BasePymongoSuite, BasePaginationTestSuite):
     @pytest.fixture(scope="session")
     def paginate_func(self, is_async_db):
         return apaginate if is_async_db else paginate
@@ -37,5 +39,42 @@ class TestPymongo(BasePaginationTestSuite):
         @builder.both.default
         async def route():
             return await maybe_async(paginate_func(db_client.test.users))
+
+        return builder.build()
+
+
+@mongodb_test
+class TestPymongoAggregate(_BasePymongoSuite, BasePaginationTestSuite):
+    @pytest.fixture(scope="session")
+    def paginate_func(self, is_async_db):
+        return apaginate_aggregate if is_async_db else paginate_aggregate
+
+    @async_fixture(scope="session")
+    async def entities(self, db_client):
+        cursor = await maybe_async(
+            db_client.test_agg.users.aggregate(
+                [
+                    {"$group": {"_id": "$name", "name": {"$first": "$name"}}},
+                    {"$sort": {"name": 1}},
+                ],
+            )
+        )
+        return await maybe_async(cursor.to_list(length=None))
+
+    @pytest.fixture(scope="session")
+    def app(self, builder, db_client, paginate_func):
+        builder = builder.new()
+
+        @builder.both.default
+        async def route():
+            return await maybe_async(
+                paginate_func(
+                    db_client.test_agg.users,
+                    [
+                        {"$group": {"_id": "$name", "name": {"$first": "$name"}}},
+                        {"$sort": {"name": 1}},
+                    ],
+                )
+            )
 
         return builder.build()
