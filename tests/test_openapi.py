eacf6ba93da9e71179324336281800d430a937ac
Yurii Karabas <1998uriyyo@gmail.com>
2025-07-26 11:46:02 +0200

    Add tests to cover openapi schema changes

---
+++ tests/test_openapi.py
@@ -0,0 +1,137 @@
+import pytest
+from fastapi import FastAPI, status
+from starlette.testclient import TestClient
+
+from fastapi_pagination import LimitOffsetPage, Page, add_pagination
+from fastapi_pagination.links import LimitOffsetPage as LinksLimitOffsetPage
+from fastapi_pagination.links import Page as LinksPage
+from fastapi_pagination.optional import OptionalLimitOffsetPage, OptionalPage
+
+app = FastAPI()
+add_pagination(app)
+
+client = TestClient(app)
+
+
+CASES = [
+    ("default", Page[int]),
+    ("limit-offset", LimitOffsetPage[int]),
+    ("links-default", LinksPage[int]),
+    ("links-limit-offset", LinksLimitOffsetPage[int]),
+    ("optional-default", OptionalPage[int]),
+    ("optional-limit-offset", OptionalLimitOffsetPage[int]),
+]
+
+for name, page in CASES:
+
+    @app.get(f"/{name}", response_model=page)
+    async def route():
+        pass
+
+
+@pytest.mark.parametrize(
+    ("endpoint", "schema"),
+    [
+        (
+            "/default",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "title": "Items", "type": "array"},
+                    "page": {"minimum": 1.0, "title": "Page", "type": "integer"},
+                    "pages": {"minimum": 0.0, "title": "Pages", "type": "integer"},
+                    "size": {"minimum": 1.0, "title": "Size", "type": "integer"},
+                    "total": {"minimum": 0.0, "title": "Total", "type": "integer"},
+                },
+                "required": ["items", "total", "page", "size", "pages"],
+                "title": "Page[int]",
+                "type": "object",
+            },
+        ),
+        (
+            "/limit-offset",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "title": "Items", "type": "array"},
+                    "limit": {"minimum": 1.0, "title": "Limit", "type": "integer"},
+                    "offset": {"minimum": 0.0, "title": "Offset", "type": "integer"},
+                    "total": {"minimum": 0.0, "title": "Total", "type": "integer"},
+                },
+                "required": ["items", "total", "limit", "offset"],
+                "title": "LimitOffsetPage[int]",
+                "type": "object",
+            },
+        ),
+        (
+            "/links-default",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "type": "array", "title": "Items"},
+                    "total": {"type": "integer", "minimum": 0.0, "title": "Total"},
+                    "page": {"type": "integer", "minimum": 1.0, "title": "Page"},
+                    "size": {"type": "integer", "minimum": 1.0, "title": "Size"},
+                    "pages": {"type": "integer", "minimum": 0.0, "title": "Pages"},
+                    "links": {"$ref": "#/components/schemas/Links", "readOnly": True},
+                },
+                "type": "object",
+                "required": ["items", "total", "page", "size", "pages", "links"],
+                "title": "Page[~TAny]Customized[int]",
+            },
+        ),
+        (
+            "/links-limit-offset",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "type": "array", "title": "Items"},
+                    "total": {"type": "integer", "minimum": 0.0, "title": "Total"},
+                    "limit": {"type": "integer", "minimum": 1.0, "title": "Limit"},
+                    "offset": {"type": "integer", "minimum": 0.0, "title": "Offset"},
+                    "links": {"$ref": "#/components/schemas/Links", "readOnly": True},
+                },
+                "type": "object",
+                "required": ["items", "total", "limit", "offset", "links"],
+                "title": "LimitOffsetPage[~TAny]Customized[int]",
+            },
+        ),
+        (
+            "/optional-default",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "type": "array", "title": "Items"},
+                    "total": {"anyOf": [{"type": "integer", "minimum": 0.0}, {"type": "null"}], "title": "Total"},
+                    "page": {"anyOf": [{"type": "integer", "minimum": 1.0}, {"type": "null"}], "title": "Page"},
+                    "size": {"anyOf": [{"type": "integer", "minimum": 1.0}, {"type": "null"}], "title": "Size"},
+                    "pages": {"anyOf": [{"type": "integer", "minimum": 0.0}, {"type": "null"}], "title": "Pages"},
+                },
+                "type": "object",
+                "required": ["items"],
+                "title": "Page[~T]Customized[int]",
+            },
+        ),
+        (
+            "/optional-limit-offset",
+            {
+                "properties": {
+                    "items": {"items": {"type": "integer"}, "type": "array", "title": "Items"},
+                    "total": {"anyOf": [{"type": "integer", "minimum": 0.0}, {"type": "null"}], "title": "Total"},
+                    "limit": {"anyOf": [{"type": "integer", "minimum": 1.0}, {"type": "null"}], "title": "Limit"},
+                    "offset": {"anyOf": [{"type": "integer", "minimum": 0.0}, {"type": "null"}], "title": "Offset"},
+                },
+                "type": "object",
+                "required": ["items"],
+                "title": "LimitOffsetPage[~T]Customized[int]",
+            },
+        ),
+    ],
+)
+def test_openapi_schema(endpoint, schema):
+    response = client.get("/openapi.json")
+    assert response.status_code == status.HTTP_200_OK
+
+    openapi_schema = response.json()
+
+    endpoint = openapi_schema["paths"][endpoint]
+    schema_ref = endpoint["get"]["responses"]["200"]["content"]["application/json"]["schema"]["$ref"]
+    *_, name = schema_ref.split("/")
+    actual_schema = openapi_schema["components"]["schemas"][name]
+
+    assert actual_schema == schema
